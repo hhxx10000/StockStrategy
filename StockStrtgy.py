f2f7a41eb0bcbd7cc37dd0d0bd85b80cd2406d18
hhxx10000 <77090302+hhxx10000@users.noreply.github.com>
2021-01-19 15:25:59 +0800

    Add files via upload

--- StockStrtgy.py
+++ StockStrtgy.py
@@ -1,10 +1,10 @@
 import GetStockData
 
 # define stock strategy
-def BuyHold(StockDate, StockData, OrigFund):
+def BuyHold(StockDate, StockData, InitFund):
     startPrice = StockData[0][0]['Open']
     endPrice = StockData[-1][-1]['Close']
-    return float(OrigFund)/startPrice*endPrice
+    return float(InitFund)/startPrice*endPrice
 
 def LazyInvest(StockDate, StockData, InitFund):
     # 懒人炒股心经：
@@ -12,29 +12,77 @@ def LazyInvest(StockDate, StockData, InitFund):
     #   早上大涨要减仓
     #   下午大涨只减仓
     #   下午大跌次日买
-    CurrCash = InitFund
-    CurrStock = 0
+    NumOfDay = len(StockData)
+    HoldCash = [InitFund] + [0] * (NumOfDay)
+    HoldStock = [0] * (NumOfDay + 1)
+    percentage = 2.0
+    BuyRatio = 0.2
+    SellRatio = 0.2
 
-    yesterday_candles = []
     for iDay, iDate in enumerate(StockDate):
-        todayStock = 1
-    print(InitCash, InitStock, TotalTime)
+        if iDay > 0:
+            yesterdayStock = StockData[iDay-1]
+        else:
+            yesterdayStock = StockData[iDay]
+        todayStock = StockData[iDay]
+
+        CurrCash = HoldCash[iDay]
+        CurrNumStock = HoldStock[iDay]
+        # 下午大跌次日买
+        if is_yesterday_afternoon_market_bearish(yesterdayStock, 100.0 - percentage):
+            # buy 20%
+            currStockPrice = todayStock[0]['Open']
+            TotalValue = currStockPrice*CurrNumStock+CurrCash
+            if CurrCash > TotalValue * BuyRatio:
+                CurrNumStock += (TotalValue * BuyRatio)//currStockPrice
+                CurrCash -= currStockPrice * CurrNumStock
+        
+        # 早上大跌要加仓
+        if is_morning_market_bearish(todayStock, 100.0 - percentage):
+            # buy 20%
+            currStockPrice = todayStock[1]["Open"]
+            TotalValue = currStockPrice*CurrNumStock+CurrCash
+            if CurrCash > TotalValue * BuyRatio:
+                CurrNumStock += (TotalValue * BuyRatio)//currStockPrice
+                CurrCash -= currStockPrice * CurrNumStock
+        
+        # 早上大涨要减仓
+        if is_morning_market_bullish(todayStock, 100.0 + percentage):
+            # sell 20%
+            currStockPrice = todayStock[1]["Open"]
+            TotalValue = currStockPrice*CurrNumStock+CurrCash
+            if CurrNumStock * currStockPrice > TotalValue * SellRatio:
+                CurrNumStock -= (TotalValue * SellRatio)//currStockPrice
+                CurrCash += currStockPrice * CurrNumStock
+        # 下午大涨只减仓
+        if is_afternoon_market_bullish(todayStock, 100.0 + percentage):
+            # sell 20%
+            currStockPrice = todayStock[-2]["Open"]
+            TotalValue = currStockPrice*CurrNumStock+CurrCash
+            if CurrNumStock * currStockPrice > TotalValue * SellRatio:
+                CurrNumStock -= (TotalValue * SellRatio)//currStockPrice
+                CurrCash += currStockPrice * CurrNumStock
+        HoldCash[iDay + 1] = CurrCash
+        HoldStock[iDay + 1] = CurrNumStock
+        print(CurrCash, CurrNumStock)
+    print(HoldCash[-1], HoldStock[-1])
+    return
 
 def is_morning_market_bearish(today_candles, percentage):
-    return today_candles[1]["open"] < today_candles[0]["open"] * percentage /100.0
+    return today_candles[1]["Open"] < today_candles[0]["Open"] * percentage /100.0
 
 def is_morning_market_bullish(today_candles, percentage):
-    return today_candles[1]["open"] > today_candles[0]["open"] * percentage /100.0
+    return today_candles[1]["Open"] > today_candles[0]["Open"] * percentage /100.0
 
 def is_yesterday_afternoon_market_bearish(yesterday_candles, percentage):
-    return yesterday_candles[-1]["close"] < yesterday_candles[-4]["open"] * percentage /100.0
+    return yesterday_candles[-1]["Close"] < yesterday_candles[-4]["Open"] * percentage /100.0
 
 def is_afternoon_market_bullish(today_candles, percentage):
-    return today_candles[-2]["open"] > today_candles[-4]["open"] * percentage /100.0
+    return today_candles[-2]["Open"] > today_candles[-4]["Open"] * percentage /100.0
 
 if __name__ == "__main__":
     Ticker = "SPY"
     StartDate = "2020-01-02"
     EndDate = "2021-01-02"
-    StockData = GetStockData.GetStockHrData(Ticker, StartDate, EndDate)
-    LazyInvest(StockData, 100000)
+    StockDate, StockData = GetStockData.GetStockHrData(Ticker, StartDate, EndDate)
+    LazyInvest(StockDate, StockData, 100000)
